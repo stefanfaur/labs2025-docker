96866ab9c7f238744a79ac580ecd19958d74abc4
sfaur <stefan.faur@irian.ro>
2025-06-06 16:18:43 +0300

    log datasource url on startup

--- backend/pizza-web/src/main/java/ro/irian/labs/pizza/LabDockerApp.java
+++ backend/pizza-web/src/main/java/ro/irian/labs/pizza/LabDockerApp.java
@@ -1,5 +1,7 @@
 package ro.irian.labs.pizza;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EntityScan(basePackages = "ro.irian.labs")
 public class LabDockerApp {
 
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(LabDockerApp.class, args);
     }
 
+    @PostConstruct
+    public void logJdbcUrl() {
+        System.out.println("▶ spring.datasource.url = " + jdbcUrl);
+    }
 }
