7f9c7056c3f48e6f6fa73e7b62d4a177c408eadc
sfaur <stefan.faur@irian.ro>
2025-06-09 19:35:27 +0300

    add multi-arch image building

--- containers/build-and-push-images.sh
+++ containers/build-and-push-images.sh
@@ -1,4 +1,27 @@
 
+# ------------------------------------------------------------------------------
+## USE THIS IF YOU ARE WORKING ON AN ARM64 MACHINE(mostly Apple Silicon Macs)
+
+# create a new builder instance
+# this is needed to build multi-arch images
+# this makes sure that the images can run on both amd64 and arm64 architectures
+docker buildx create --name multiarch-builder --use
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --tag sfaur/labs-pizza-backend:latest:latest \
+  --push \
+  ../backend
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --tag sfaur/labs-pizza-frontend:latest \
+  --push \
+  ../backend
+
+# ------------------------------------------------------------------------------
+## USE THIS IF YOU ARE WORKING ON AN AMD64 MACHINE(pretty much everything else)
+
 # go to the root of the backend containing the Dockerfile
 cd ../backend
 # build the image locally and tag as latest
